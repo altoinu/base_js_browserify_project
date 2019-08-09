01adedc0be810125da0a52b6c690a0c009511b7d
Kaoru Kawashima <altoinu@users.noreply.github.com>
2019-08-09 17:48:13 -0400

    jsdoc stuff

--- README.md
+++ README.md
@@ -2,13 +2,26 @@ Initial set up
 - npm install
 - bower install (or npm run bower install)
 
-run this to compile js
+run this to compile js and doc
 - npm run build<br/>
-outputs to dist/[output file].js, dist/[output file].min.js
+   - outputs to dist/[output file].js, dist/[output file].min.js
+   - outputs documentation to dist/jsdoc
 
-run this to compile jsdoc
-- npm run jsdoc<br/>
-outpus to dist/jsdoc/
+run this to compile only jsdoc
+- npm run build:doc:jsdoc<br/>
+   - outpus to dist/jsdoc
+- jsdoc
+   - https://www.npmjs.com/package/jsdoc
+   - https://jsdoc.app/
+      - https://jsdoc.app/about-getting-started.html
+      - https://jsdoc.app/tags-example.html
+      - Namepaths https://jsdoc.app/about-namepaths.html
+      - Namespace https://jsdoc.app/tags-namespace.html
+         - memberof https://jsdoc.app/tags-memberof.html
+      - Module https://jsdoc.app/tags-module.html
+         - param https://jsdoc.app/tags-param.html
+         - Function https://jsdoc.app/tags-function.html
+         - Class https://jsdoc.app/tags-class.html
 
 TODO: ?
 package.json- build:js:collapse
--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-module.exports = {
-	data: 'hello'
-};
+/**
+ * Sample class
+ * @class
+ * @classdesc Sample class description here
+ * @param {string} text
+ * @param {string} [text2]
+ */
+function MainClass(text, text2) {
+
+	// --------------------------------------------------------------------------
+	//
+	// public properties
+	//
+	// --------------------------------------------------------------------------
+
+	this.data = 'hello';
+
+	this.text = text;
+
+	/**
+	 * @default 'good bye'
+	 */
+	this.text2 = text2 ? text2 : 'good bye';
+
+	// --------------------------------------------------------------------------
+	//
+	// private functions
+	//
+	// --------------------------------------------------------------------------
+
+	function sayItNow(name) {
+
+		alert(this.data + ' ' + name + '!');
+
+	}
+
+	// --------------------------------------------------------------------------
+	//
+	// public methods
+	//
+	// --------------------------------------------------------------------------
+
+	/**
+	 * Alerts message with this.data and specified name.
+	 * @param {string} name Name to alert.
+	 * @returns {string} 'asdf' as example.
+	 */
+	this.sayHello = function(name) {
+
+		sayItNow(name);
+
+		return 'asdf';
+
+	};
+
+}
+
+module.exports = MainClass;
